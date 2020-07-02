e06edecf477ba62b1d2e75a8dbdf5317479150c0
James Mortensen <james.mortensen@anywhere.co>
2020-07-02 10:28:31 +0530

    Commented out the JDO module to deploy an HTTPS test to check the status code of http to https redirects

--- multi-service/service1/src/main/java/com/loosebazooka/test/MyServlet.java
+++ multi-service/service1/src/main/java/com/loosebazooka/test/MyServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.sample.*;
+//import com.sample.*;
 
 
 
@@ -16,8 +16,8 @@ public class MyServlet extends HttpServlet {
   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     resp.setContentType("text/plain");
     String content = "this is the test servlet from service 1 using 3.1 annotations";
-    HelloWorld helloWorld = new HelloWorld();
-    content += "\n From JDO-service: " + helloWorld.getHelloWorld();
+    //HelloWorld helloWorld = new HelloWorld();
+    //content += "\n From JDO-service: " + helloWorld.getHelloWorld();
     resp.getWriter().println(content);
   }
 }
--- multi-service/service2/src/main/java/com/loosebazooka/test/MyServlet.java
+++ multi-service/service2/src/main/java/com/loosebazooka/test/MyServlet.java
@@ -3,7 +3,7 @@ package com.loosebazooka.test;
 import java.io.IOException;
 import javax.servlet.http.*;
 
-import com.sample.*;
+//import com.sample.*;
 
 public class MyServlet extends HttpServlet {
     @Override
@@ -11,8 +11,8 @@ public class MyServlet extends HttpServlet {
             throws IOException {
         resp.setContentType("text/plain");
         String content = "this is the test servlet from service 2 using web.xml";
-        HelloWorld helloWorld = new HelloWorld();
-        content += "\n From JDO-service: " + helloWorld.getHelloWorld();
+        //HelloWorld helloWorld = new HelloWorld();
+        //content += "\n From JDO-service: " + helloWorld.getHelloWorld();
         resp.getWriter().println(content);
     }
 }
